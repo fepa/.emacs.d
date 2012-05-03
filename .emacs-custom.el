5ee14b8c0ba1fb341683cdb4b37c0c093524f951
Felix Panozzo <fpanozzo@gmail.com>
2012-05-03 13:55:19 +0200

    customs file updated

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,7 +3,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40"))
+ '(ecb-options-version "2.40")
+ '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/home/fepa/Projecs/beatrice/bin/python" :settings "beatrice.settings.development")))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.

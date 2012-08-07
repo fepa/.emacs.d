0324e9696a867b27bca615dcce93f081e8bf4ba4
Felix Panozzo <fpanozzo@gmail.com>
2012-08-07 16:42:37 +0200

    disabled compile at save for scss-mode

--- init.el
+++ init.el
@@ -177,4 +177,5 @@
 (add-to-list 'load-path scss-path)
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
 (require 'scss-mode)

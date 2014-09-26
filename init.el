b02d89adad759f9165acaff3d26cb94d4c1e3f31
Felix Panozzo <fpanozzo@gmail.com>
2014-09-26 23:45:40 +0200

    Added smooth-scrolling

--- init.el
+++ init.el
@@ -153,3 +153,8 @@
 (put 'upcase-region 'disabled nil)
 
 (require 'iso-transl) ;; Fixes typing accent characters
+
+(require 'smooth-scrolling)
+(setq smooth-scroll-margin 5)
+(setq scroll-conservatively 9999
+      scroll-preserve-screen-position t)
